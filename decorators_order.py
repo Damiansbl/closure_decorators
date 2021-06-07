f1036077b911027065d7e896863b49ba979aae70
Damian Sobol <damiansbl@gmail.com>
2021-06-07 19:46:17 +0200

    more advanced examples

---
+++ decorators_order.py
@@ -0,0 +1,21 @@
+def upper(fn):
+    def inner(name):
+        return fn(name.title())
+
+    return inner
+
+
+def reverse(fn):
+    def inner(name):
+        return fn(name)[::-1]
+
+    return inner
+
+
+@reverse
+@upper
+def hello(name):
+    return f"Hello {name}"
+
+
+print(hello('pawel'))
